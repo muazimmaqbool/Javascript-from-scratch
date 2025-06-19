d8b3f597c624c73b2705d163fe2b24c042471c3a
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-06-19 13:58:40 +0530

    Abstraction example

--- OOPS/03_Abstraction.js
+++ OOPS/03_Abstraction.js
@@ -1,1 +1,19 @@
-//
+//Abstraction:
+/*
+Here Objects only reveal internal mechanisms that are relevant for the use of other objects, hiding any unnecessary
+implementation code. 
+The derived class can have its functionality extended. This concept can help developers more easily make additional 
+changes or additions over time.
+*/
+//Example:
+class Car {
+  startEngine() {
+    console.log("Engine started");
+  }
+}
+
+const myCar = new Car();
+myCar.startEngine(); // We don't need to know how it starts, just that it does
+
+
+//node --watch OOPS\03_Abstraction.js
