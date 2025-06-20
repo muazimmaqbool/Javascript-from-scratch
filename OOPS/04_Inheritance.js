e113dedce53139fa904a43a8a6c63e8cf80b7c04
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-06-20 15:04:20 +0530

    inheritance

---
+++ OOPS/04_Inheritance.js
@@ -0,0 +1,8 @@
+//Inheritance:
+/*
+Inheritance lets one object acquire the properties and methods of another object. 
+In JavaScript this is done by Prototypal Inheritance.
+Reusability is the main benefit here. We know sometimes that multiple places need to do the same thing, and 
+they need to do everything the same except for one small part. This is a problem inheritance can solve.
+*/
+//
