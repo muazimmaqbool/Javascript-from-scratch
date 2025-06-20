103642960bbde4bd8375206de968f05063358b28
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-06-20 15:07:52 +0530

    Inheritance example

--- OOPS/04_Inheritance.js
+++ OOPS/04_Inheritance.js
@@ -1,8 +1,26 @@
 //Inheritance:
 /*
 Inheritance lets one object acquire the properties and methods of another object. 
-In JavaScript this is done by Prototypal Inheritance.
 Reusability is the main benefit here. We know sometimes that multiple places need to do the same thing, and 
 they need to do everything the same except for one small part. This is a problem inheritance can solve.
 */
-//
+//Example:
+class Car {
+  Break() {
+    console.log("Car Stop On Break Press!");
+  }
+}
+
+class Bike extends Car {
+  speed() {
+    console.log("Bike has fast speed!");
+  }
+}
+
+const d = new Bike();
+d.Break(); // Inherited from Car class
+d.speed();
+
+
+
+//Run this code in terminal via this command: node --watch OOPS\04_Inheritance.js
