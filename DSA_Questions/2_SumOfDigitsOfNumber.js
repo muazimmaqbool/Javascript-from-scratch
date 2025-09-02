558e4cdd362b21a6005ab115f66ed43f3c256b23
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-09-02 22:20:50 +0530

    Sum of digits of a number

--- DSA_Questions/2_SumOfDigitsOfNumber.js
+++ DSA_Questions/2_SumOfDigitsOfNumber.js
@@ -3,5 +3,16 @@
 Q4: Sum of digits of a number?
 
 Example 1287 : 1+2+8+7=18
-
+How to calculate it we need to divide this number by 10:
+    Until the number is > 0
 */
+function sumDigits(num){
+    //here we didn't know the number of steps that's why we use while loop
+    let sum=0;
+    while(num>0){
+        sum+=num%10; //Remainder
+        num=num/10 //Divide quotient
+    }
+    return Math.floor(sum)
+}
+console.log("Sum of 1287:",sumDigits(1287))
