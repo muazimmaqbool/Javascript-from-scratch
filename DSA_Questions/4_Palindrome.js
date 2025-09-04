9affbee5ef56e52cfb91b16942bcfc3627a986ea
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-09-04 21:43:58 +0530

    palindrome num info

---
+++ DSA_Questions/4_Palindrome.js
@@ -0,0 +1,12 @@
+//->To run this use: node --watch DSA_Questions\4_Palindrome.js
+/*
+Q: Given an integer n, return true if it is a palindrome number and false otherwise?
+
+The palindrome number is the number whichi read same in both ways i.e form start and from back
+example 121 is read same from front and back
+example 1234 is not a palindrom because it is not read same on both fron and back
+
+To solve first found reverse of a given number then check if the reverse of then umber is equal to the the original number
+if it is then its palindrome number and if not then it is not
+
+*/
