e9f53cab21b0f04b1399c26fdd60c904ffa0b830
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-09-06 23:11:58 +0530

    code added

--- DSA/1 _Time_and_Space_Complexity/2_TimeComplexity.js
+++ DSA/1 _Time_and_Space_Complexity/2_TimeComplexity.js
@@ -1,1 +1,13 @@
-//To Run thi: node --watch DSA\1_Time_and_Space_Complexity\2_TimeComplexity.js
+//To run this: node --watch DSA\1_Time_and_Space_Complexity\2_TimeComplexity.js
+
+const randomArray = [12, 56, 74, 85, 41, 26, 45, 88];
+//Example with print start time and end time
+const stopOnAny = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (i === 3) return arr[i];
+  }
+};
+//also measuring how much time it takes
+console.time("start");
+console.log("result:", stopOnAny(randomArray));
+console.timeEnd("end");
