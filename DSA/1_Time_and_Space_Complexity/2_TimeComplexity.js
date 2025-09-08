1bbe7db30040b10ca3301ba78dcb73300ee85f4c
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-09-08 22:34:18 +0530

    measure of effeciency of an algorithm

--- DSA/1_Time_and_Space_Complexity/2_TimeComplexity.js
+++ DSA/1_Time_and_Space_Complexity/2_TimeComplexity.js
@@ -28,7 +28,11 @@ console.timeEnd("executionTime");
 //Here the exection time of getNum function will be less the the stopOnAny function as inside stopOnAny we are using loops
 /*
 ->But this is not the better way to determine the performance of any alogrithm because it will also depend on the machine so we can use operations
-  to measure the effeciency of an alogrithm, so these operations means how many tasks an algorithm is performing
-  
+  to measure the effeciency of an alogrithm, so these operations means how many tasks an algorithm is performing:
+  1: So in function 'stopOnAny' we have task i.e if (i === 3) return arr[i]; so this task will run number of times for loop runs
+     so in our case for loop run from 0 till 3 i.e 4 times so we have 4 tasks in 'stopOnAny' function
+
+  2: In 'getNum' function we are just returning the first element of any array so it was only task and it will only run once
+
 
 */
