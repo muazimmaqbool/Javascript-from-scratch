12742e276459bdb2ed41f19e7aec14f94f99c7e2
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-09-09 13:17:08 +0530

    space complexity intro

---
+++ DSA/1_Time_and_Space_Complexity/6_SpaceComplexity.js
@@ -0,0 +1,9 @@
+//To run this go inside this folder via cd and node --watch 6_TimeComplexity.js
+
+/* 
+Space Complexity: it's basically an amount of memoty an algorithm needs to run and it's also measured in big O notation
+space and memory is basically the memory taken by the variables which are decalred like strings, array , int etc and that's exactly
+what space compelxity is,
+So, basically as the input size increases so does the soace complexity and keep in mind it's not always about the size of input but
+also the size of the algorithm and any other additional data structure if used
+*/
