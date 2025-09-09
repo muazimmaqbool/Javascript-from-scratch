5ace055ec950738e1409300fc7286578ad31f7d0
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-09-09 09:49:12 +0530

    extra info

--- DSA/1_Time_and_Space_Complexity/3_TimeComplexity.js
+++ DSA/1_Time_and_Space_Complexity/3_TimeComplexity.js
@@ -1,7 +1,8 @@
 //To run this go inside this folder via cd and node --watch 3_TimeComplexity.js
 
-//Example someOperations:
+//Example someOperations: O(1)
 function someOperations(n){
+    //3 operations
     return (n*(n+5))/2;
 }
 console.log(someOperations(2))
@@ -10,4 +11,6 @@ console.log(someOperations(100))
 /*Here this function/algorithm have timeComlexity of O(1) because it was only one operation i.e constant number of operations i.e 3
 doesn't matter the number of inputs i.e n
  Here the someOperations function have 3 operations i.e n*  is one, n+5 is two and /2 is third
+
+ So this algorithm will take same amount of time with each and every call
 */
