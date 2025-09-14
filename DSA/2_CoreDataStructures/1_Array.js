2b57c67ddfa4a024c16033565f2b0edab57efe7e
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-09-14 12:52:49 +0530

    Array introduction

--- DSA/2_CoreDataStructures/1_Array.js
+++ DSA/2_CoreDataStructures/1_Array.js
@@ -1,1 +1,12 @@
 //To run go inside this folder via cd and enter: node --watch 1_Array.js (--watch is optional)
+/*
+Array introduction:
+    An array is a collection of items of same data type that are stored at contiguous memory locations. 
+    It is one of the most popular and simple data structures used in programming.
+
+    Note:A contiguous memory location is a sequence of memory blocks that are adjacent to each other in a single, continuous block, allowing for efficient and fast access to data
+    
+    - Basic terminologies of Array:
+        Array Element: Elements are items stored in an array.
+        Array Index: Elements are accessed by their indexes. Indexes in most of the programming languages start from 0.
+*/
