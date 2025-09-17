3332e8674eae10263323f8e0fd440cc01826d787
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-09-17 13:08:09 +0530

    subarray output

--- DSA/2_CoreDataStructures/2_Array.js
+++ DSA/2_CoreDataStructures/2_Array.js
@@ -1,6 +1,8 @@
 //To run go inside this folder via cd and enter: node --watch 2_Array.js (--watch is optional)
 
 //Q: sub-arrays all possible:
+//This function is designed to return only contiguous subarrays (i.e. elements next to each other in the original array)
+// that means [1,3] will not be returned
 function subarrays(arr) {
   let result = [];
   for (let i = 0; i < arr.length; i++) {
@@ -14,3 +16,4 @@ function subarrays(arr) {
 }
 
 console.log(subarrays([1,2,3]));
+// o/p: [ [ 1 ], [ 1, 2 ], [ 1, 2, 3 ], [ 2 ], [ 2, 3 ], [ 3 ] ]
