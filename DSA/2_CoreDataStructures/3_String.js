83aefbd50d0728cfca6d04ebeabf09fcf473e23e
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-09-19 19:50:27 +0530

    palindrome exaple

--- DSA/2_CoreDataStructures/3_String.js
+++ DSA/2_CoreDataStructures/3_String.js
@@ -8,4 +8,30 @@ String: In programming, a string is a fundamental data type used to represent se
 
 //String Manipulation:
 
-//1:
+//1:Palindrome
+function isPalindrome(str) {
+  let left = 0, right = str.length - 1;
+//   console.log("right:",right)
+  while(left < right){
+    //if characters at the left and right positions don’t match, it’s not a palindrome → return false immediately.
+    if(str[left] !== str[right]) return false;
+    left++; right--;
+  }
+  return true;
+}
+
+console.log(isPalindrome("mom")); // true
+console.log(isPalindrome("hello")); //false
+/*
+Example with "mom":
+    left=0 (m), right=2 (m) → match → left=1, right=1.
+    Loop ends (left not < right anymore).
+    Return true.
+
+Example with "hello":
+    left=0 (h), right=4 (o) → mismatch → return false immediately.
+
+Why it works:
+    A palindrome reads the same forward and backward.
+    By comparing from the outside inward, you can detect mismatches early without reversing the string.
+*/
