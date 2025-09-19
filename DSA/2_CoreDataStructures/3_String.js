cdb7e0e597b543b38db0930e9fb9dcb5c84c232f
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-09-19 18:04:59 +0530

    String definition

---
+++ DSA/2_CoreDataStructures/3_String.js
@@ -0,0 +1,11 @@
+//To run go inside this folder via cd and enter: node --watch 3_String.js
+
+/*
+String: In programming, a string is a fundamental data type used to represent sequences of characters. 
+        These characters can include letters, numbers, symbols, and whitespace. Strings are essentially text data that 
+        can be stored, manipulated, and displayed within a program.
+*/
+
+//String Manipulation:
+
+//1:
