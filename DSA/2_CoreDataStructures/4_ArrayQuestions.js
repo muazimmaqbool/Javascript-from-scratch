7bf74944b920d3697d62797d683a09a550201ca6
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-09-23 22:10:56 +0530

    brute force approach

--- DSA/2_CoreDataStructures/4_ArrayQuestions.js
+++ DSA/2_CoreDataStructures/4_ArrayQuestions.js
@@ -1,9 +1,18 @@
 //To run go inside this folder via cd and enter: node --watch 4_ArrayQuestions.js
 
+//Important:
+/*
+->What is Brute Force Approach?
+  A brute force approach is a straightforward problem-solving method that systematically tries every possible solution until
+  the correct one is found. It's a simple, exhaustive trial-and-error technique, but it can be very inefficient and slow for 
+  complex problems due to the sheer number of possibilities it must check.
+*/
+
 // Q1: Second largest number
 /*
-->Given an array of size N, print second largest distinct element from an array
+->Given an array of size N, print second largest element from an array
   Example:
    -> input: [12,35,2,1,34] : o/p = 34
    -> input: [10,5,10] : o/p =5
 */
+//First solvingi it using brute force approach:
