35cf3e76f619844e6d596a5398da55891c688a05
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-09-23 14:00:19 +0530

    name update

---
+++ Javascript_from_scratch/79_every.js
@@ -0,0 +1,5 @@
+//to run this go to inside this folder via cd and run: node --watch 79_every.js
+
+//The every() method is an iterative method used with arrays. 
+//It checks whether all elements in an array pass a specific test provided by a callback function.
+
