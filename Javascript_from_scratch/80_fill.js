74832129e3cdabbef9b46c790156eef5e53ffa2e
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-09-23 14:05:08 +0530

    fill in js info

---
+++ Javascript_from_scratch/80_fill.js
@@ -0,0 +1,12 @@
+//to run this go to inside this folder via cd and run: node --watch 80_fill.js
+
+/*
+The Array.prototype.fill() method in JavaScript is used to change all elements in an array to a static value, 
+    from a start index to an end index.
+
+The fill() method modifies the original array and returns the modified array. You can use it in a few ways:
+    arr.fill(value): This fills the entire array with the specified value.
+    arr.fill(value, start): This fills the array with the value from the start index up to the end of the array.
+    arr.fill(value, start, end): This fills the array with the value from the start index up to, but not including, the end index.
+
+*/
