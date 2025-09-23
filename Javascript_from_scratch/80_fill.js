a5477da61fb28c2b8b4efc70b783738ae8defe53
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-09-23 14:12:37 +0530

    fill example

--- Javascript_from_scratch/80_fill.js
+++ Javascript_from_scratch/80_fill.js
@@ -10,3 +10,17 @@ The fill() method modifies the original array and returns the modified array. Yo
     arr.fill(value, start, end): This fills the array with the value from the start index up to, but not including, the end index.
 
 */
+// 1: Filling the Entire Array
+const numbers = [1, 2, 3, 4, 5];
+numbers.fill(0);
+console.log(numbers); // Output: [0, 0, 0, 0, 0]
+
+// 2. Filling a Specific Segment 
+const numbersTwo = [1, 2, 3, 4, 5];
+numbersTwo.fill(10, 2, 4); //the array is filled with the number 10 starting at index 2 and ending before index 4 i.e 3 and 4 will be replaced by 10
+console.log(numbersTwo); // Output: [1, 2, 10, 10, 5]
+
+// 3. Initializing an Array
+//fill() is particularly useful for creating and initializing an array with a fixed size and value.
+const newArray = new Array(5).fill(7);
+console.log(newArray); // Output: [7, 7, 7, 7, 7]
