a0f394d4120e2ad2ab30866edc1146b5e9886012
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-09-23 14:02:36 +0530

    every in js

--- Javascript_from_scratch/79_every.js
+++ Javascript_from_scratch/79_every.js
@@ -3,3 +3,12 @@
 //The every() method is an iterative method used with arrays. 
 //It checks whether all elements in an array pass a specific test provided by a callback function.
 
+const numbers = [2, 4, 6, 8, 10];
+
+const areAllEven = numbers.every((e)=>e%2===0)
+console.log(areAllEven); // Output: true
+
+const numbersWithOdd = [2, 4, 6, 7, 10];
+
+const areAllEven2 = numbersWithOdd.every((e)=>e%2===0);
+console.log(areAllEven2); // Output: false
