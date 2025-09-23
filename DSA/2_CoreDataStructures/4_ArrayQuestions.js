39a862cdfe4251fcabaaa4f6fe212ca57b46c880
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-09-23 21:35:45 +0530

    Array questions

---
+++ DSA/2_CoreDataStructures/4_ArrayQuestions.js
@@ -0,0 +1,9 @@
+//To run go inside this folder via cd and enter: node --watch 4_ArrayQuestions.js
+
+// Q1: Second largest number
+/*
+->Given an array of size N, print second largest distinct element from an array
+  Example:
+   -> input: [12,35,2,1,34] : o/p = 34
+   -> input: [10,5,10] : o/p =5
+*/
