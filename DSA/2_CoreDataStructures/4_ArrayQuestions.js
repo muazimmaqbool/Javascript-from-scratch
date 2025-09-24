816880ee0f2709d184849448130e35ab4d312dcb
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-09-24 22:18:14 +0530

    solution

--- DSA/2_CoreDataStructures/4_ArrayQuestions.js
+++ DSA/2_CoreDataStructures/4_ArrayQuestions.js
@@ -16,3 +16,15 @@
    -> input: [10,5,10] : o/p =5
 */
 //First solvingi it using brute force approach:
+
+/*-> if we sort the array then return second last element it will be second largest element:
+  [12,35,2,1,34] -- sort() ---> [1,2,12,34,35] -- > return second last i.e 34
+  But when we have array like this [10,5,10] here comes the problem if we sort it
+    [10,5,10] --- sort() ---> [5,10,10] --> returns second last i.e 10 which is not second largest
+    so to resolve this issue we will first remove repeated elements from the array and then return second last element it will be 
+    second largest element.
+*/
+
+const secondLargestNumber=(arr)=>{
+  const
+}
