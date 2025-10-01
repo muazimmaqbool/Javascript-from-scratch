04b6443f07017e3b04bad588fdb467a349fa857f
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-10-01 22:15:33 +0530

    solution 1

--- DSA/2_CoreDataStructures/5_ArrayQuestionTwo.js
+++ DSA/2_CoreDataStructures/5_ArrayQuestionTwo.js
@@ -7,5 +7,19 @@
     
    Example:
     -> input: nums=[1,2,3,4,5,6,7], k=3 >>>>> o/p = [5,6,7,1,2,3,4]
+        i.e: [1,2,3,4,5,6,7] >> [7,1,2,3,4,5,6] >> [6,7,1,2,3,4,5] >> [5,6,7,1,2,3,4]
     -> input: nums=[-1,-100,3,99], k=2 >>>>>> o/p = [3,99,-1,-100]
+
 */
+//Solution 1:
+const rotateArray=(nums,k)=>{ //nums is array and k is no. of rotation
+     let size=nums.length
+     //making sure k is not more then size of nums
+     if(size>k){
+        k=k%size // i.e if k is 8 and nums length is 7 i.e 10 % 7 i.e reminder is 3, 
+     }
+     const rotated=nums.splice(size-k,size);
+     nums.unshift(...rotated)
+     return nums
+}
+console.log(rotateArray([1,2,3,4,5,6,7],3))
