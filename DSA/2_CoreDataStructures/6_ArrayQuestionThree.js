2a1b84f97df4c4d5e2660f59714af3013f5a6cca
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-10-07 22:24:36 +0530

    fix

--- DSA/2_CoreDataStructures/6_ArrayQuestionThree.js
+++ DSA/2_CoreDataStructures/6_ArrayQuestionThree.js
@@ -6,6 +6,6 @@ Give an integer array which is in non-decreasing order, remove the duplicates in
 such that each unique element appears only once. 
 The relative order of the elements should be kept the same, Then return the number of unique elements in array.
 
-    -> input: [1,1,2]  >>>>>>>>> o/p: 2, [1,2]
-    -> input: [0,0,1,1,2,2,3,3,4] >>>>>>>>> o/p: 5, [0,1,2,3,4]
+    -> input: [1,1,2]  >>>>>>>>> o/p: 2, [1,2,_]
+    -> input: [0,0,1,1,2,2,3,3,4] >>>>>>>>> o/p: 5, [0,1,2,3,4,_,_,_,_,_]
 */
