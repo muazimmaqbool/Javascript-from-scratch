ed5b0fa1a523e06867807ba65ed36f3e9888a811
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-10-07 22:22:38 +0530

    new array question setup

---
+++ DSA/2_CoreDataStructures/6_ArrayQuestionThree.js
@@ -0,0 +1,10 @@
+//To run go inside this folder via cd and enter: node --watch 6_ArrayQuestionThree.js
+
+//Q3: Remove duplicate from sorting array:
+/*
+Give an integer array which is in non-decreasing order, remove the duplicates in place such that each unique element appears
+only once. The relative order of the elements should be kept the same, Then return the number of unique elements in array.
+
+    -> input: [1,1,2]  >>>>>>>>> o/p: 2, [1,2]
+    -> input: [0,0,1,1,2,2,3,3,4] >>>>>>>>> o/p: 5, [0,1,2,3,4]
+*/
