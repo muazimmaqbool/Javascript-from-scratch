cf124f0a66e735e2271aa21210e76cb995035acb
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-10-07 22:23:42 +0530

    question updated

--- DSA/2_CoreDataStructures/6_ArrayQuestionThree.js
+++ DSA/2_CoreDataStructures/6_ArrayQuestionThree.js
@@ -2,8 +2,9 @@
 
 //Q3: Remove duplicate from sorting array:
 /*
-Give an integer array which is in non-decreasing order, remove the duplicates in place such that each unique element appears
-only once. The relative order of the elements should be kept the same, Then return the number of unique elements in array.
+Give an integer array which is in non-decreasing order, remove the duplicates in-place(means don't create new array) 
+such that each unique element appears only once. 
+The relative order of the elements should be kept the same, Then return the number of unique elements in array.
 
     -> input: [1,1,2]  >>>>>>>>> o/p: 2, [1,2]
     -> input: [0,0,1,1,2,2,3,3,4] >>>>>>>>> o/p: 5, [0,1,2,3,4]
