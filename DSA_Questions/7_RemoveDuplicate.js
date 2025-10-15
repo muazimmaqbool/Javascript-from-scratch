85bbbfa7fe69e4597dac9d1b22426b894d694662
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-10-15 21:15:28 +0530

    remove duplcates

--- DSA_Questions/7_RemoveDuplicate.js
+++ DSA_Questions/7_RemoveDuplicate.js
@@ -6,6 +6,26 @@ Give an integer array which is in non-decreasing order, remove the duplicates in
 such that each unique element appears only once. 
 The relative order of the elements should be kept the same, Then return the number of unique elements in array.
 
-    -> input: [1,1,2]  >>>>>>>>> o/p: 2, [1,2,_]
-    -> input: [0,0,1,1,2,2,3,3,4] >>>>>>>>> o/p: 5, [0,1,2,3,4,_,_,_,_,_]
+    -> input: [1,1,2]  >>>>>>>>> o/p: 2 here o/p should be 2 i.e remaining elements after removing duplicates i.e [1,2]
+    -> input: [0,0,1,1,2,2,3,3,4] >>>>>>>>> o/p: 5 i.e remaining relements after removing duplicates are [0,1,2,3,4]
 */
+//Solution 1 with builtin methods/functions
+/*
+Eg: nums=[0,0,1,1,2,2,3,3,4] : so we will compare first two elements i.e 0 and 0 and if they are equal we will remove second one using splice i.e 
+                              nums.splice(i+1,1)
+                              i.e we are iterating over nums and compareing first two i.e arr[0] and arr[1]
+                               i.e 0 and 0 so we are removing second 0 i.e i+1th position removing 1 element, and after removing it we will
+                            check again 0 and 1 and if they are no equal and i++ so it checks second and third element and so on
+*/
+console.log("Solution One:")
+const removeDuplicates=(nums)=>{
+    for(let i=0;i<nums.length-1;i++){
+        if(nums[i]===nums[i+1]){
+            nums.splice(i+1,1);
+            i--
+        }
+    }
+    return nums.length
+}
+console.log(removeDuplicates([0,0,1,1,2,2,3,3,4])) // 5
+console.log(removeDuplicates([1,1,2])) // 2
