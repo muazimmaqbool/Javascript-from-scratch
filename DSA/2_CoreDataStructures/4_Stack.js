d87e008aaf3e1df200b4a2c1cf211c0ccf5cc43d
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-10-18 12:47:51 +0530

    Stack info

---
+++ DSA/2_CoreDataStructures/4_Stack.js
@@ -0,0 +1,16 @@
+//To run go inside this folder via cd and enter: node --watch 4_Stack.js
+/*
+->Stack Data Structure:
+    A stack is a linear data structure that follows the Last-In, First-Out (LIFO) principle. 
+    This means the last element added to the stack is the first one to be removed. 
+    Imagine a stack of plates: you can only add a new plate to the top, and you can only remove the top plate. 
+
+    Stack Operations:
+    | Operation   | Description             |
+    | ----------- | ----------------------- |
+    | `push()`    | Add an item on top      |
+    | `pop()`     | Remove the top item     |
+    | `peek()`    | View the top item       |
+    | `isEmpty()` | Check if stack is empty |
+    | `size()`    | Get number of items     |
+*/
