b990b67faebd1bf939bbd8c407c75c908e06080e
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-10-24 21:18:37 +0530

    Queue intro

---
+++ DSA/2_CoreDataStructures/5_Queue.js
@@ -0,0 +1,18 @@
+//To run go inside this folder via cd and enter: node --watch 5_Queue.js
+/*
+->Queue Data Structure:
+ A queue is a linear data structure that follows the First-In, First-Out (FIFO) principle. 
+ This means that the first element added to the queue is the first one to be removed, similar to a line of people waiting for a service. 
+ A Queue is like a line at a coffee shop — first person in line gets served first.
+
+ -> QUeue Operations:
+
+    | Operation   | Description            |
+    | ----------- | ---------------------- |
+    | `enqueue()` | Add to the rear        |
+    | `dequeue()` | Remove from the front  |
+    | `peek()`    | View front element     |
+    | `isEmpty()` | Check if empty         |
+    | `size()`    | Get number of elements |
+
+*/
