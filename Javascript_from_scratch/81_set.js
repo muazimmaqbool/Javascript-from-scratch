9d870244c2b41bbbced074d736a1061753fc8ed3
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-10-25 15:47:46 +0530

    operators on sets like conversion and quick tips

--- Javascript_from_scratch/81_set.js
+++ Javascript_from_scratch/81_set.js
@@ -33,5 +33,33 @@ console.log("deleting banana...")
 fruits.delete("Banana")
 console.log("fruits:",fruits)
 console.log("clear all")
-fruits.clear()
-console.log("fruits:",fruits) // o/p: Set(0) {}
+
+// fruits.clear()
+//console.log("fruits:",fruits) // o/p: Set(0) {} //checkout this after fruits.clear()
+
+//->Looping Through a Set
+fruits.add("mango")
+fruits.add("watermellon")
+fruits.add("grapes")
+for(const i of fruits){
+    console.log(i)
+}
+//or
+fruits.forEach((val)=>console.log(val))
+
+//->Convert Between Set and Array
+//set to array
+console.log("fruits:",fruits) // o/p: Set(5) { 'Apple', 'Orange', 'mango', 'watermellon', 'grapes' }
+const arr=[...fruits] // or Array.from(fruits)
+console.log("arr:",arr) // o/p: [ 'Apple', 'Orange', 'mango', 'watermellon', 'grapes' ]
+//Array to set
+const uniqueArr=new Set(['a','b','a','c']) //it will also remove duplicate
+console.log("unique arr:",uniqueArr) // o/p: Set(3) { 'a', 'b', 'c' }
+
+//->Common Use Cases
+    //1) Remove duplicates from an array
+    //2) Checks if any existing elements quickly
+//->Important Notes
+// 1) Order of elements is insertion order.
+// 2) Unlike arrays, Sets do not support indexing (e.g. mySet[0] won’t work).
+// 3) Each value in a Set must be unique.
