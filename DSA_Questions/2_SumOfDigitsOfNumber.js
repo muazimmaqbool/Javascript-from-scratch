19c56a99ae6866b3574c66e73ae4b9a80c67aafc
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-11-03 21:46:28 +0530

    question two

--- DSA_Questions/2_SumOfDigitsOfNumber.js
+++ DSA_Questions/2_SumOfDigitsOfNumber.js
@@ -1,7 +1,7 @@
 /*
-->To run this use: node --watch DSA_Questions\2_SumOfDigitsOfNumber.js
-Q4: Sum of digits of a number?
+->To run this use: node --watch 2_SumOfDigitsOfNumber.js
 
+Q4: Sum of digits of a number?
 Example 1287 : 1+2+8+7=18
 How to calculate it we need to divide this number by 10:
     Until the number is > 0
@@ -16,3 +16,8 @@ function sumDigits(num){
     return Math.floor(sum)
 }
 console.log("Sum of 1287:",sumDigits(1287))
+
+//Time Complexity is : O(log n)
+//Since the input size N is being repeatedly divided by a constant (10) until it reaches zero, the number of steps grows logarithmically with the input value $N$
+
+//Space Complexity is: O(1)
