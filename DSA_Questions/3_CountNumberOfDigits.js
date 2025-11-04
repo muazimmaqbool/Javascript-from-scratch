c28fb00b81ea255352ab61d22d97294df065e27b
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-11-04 18:56:58 +0530

    count digits

--- DSA_Questions/3_CountNumberOfDigits.js
+++ DSA_Questions/3_CountNumberOfDigits.js
@@ -1,9 +1,9 @@
 /*
-->To run this use: node --watch DSA_Questions\3_CountNumberOfDigits.js
+->To run this use: node --watch 3_CountNumberOfDigits.js
 Q4: Count the number of digits of a number ?
 
 Example: count 34252 i.e 5 digits and for -121 we have 3 digits
-and we get it we need to divide the number by 10 until it reaches 0
+and to get it we need to divide the number by 10 until it reaches 0
 */
 function numDigits(num) {
   num = Math.abs(num); // takinng absolute values only i.e -121 becomes 121
@@ -18,5 +18,7 @@ function numDigits(num) {
   return count;
 }
 
-console.log("digits in 34252 are:", numDigits(34252)); 
-console.log("digits in -121 are:", numDigits(-121));
+console.log("digits in 34252 are:", numDigits(34252)); // 5
+console.log("digits in -121 are:", numDigits(-121)); // 3
+console.log("digits in 99555222 are:", numDigits(99555222)); // 8
+
