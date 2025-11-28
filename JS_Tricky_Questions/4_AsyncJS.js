456a039c49d5e559d2c31af0344a6272714d780f
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-11-28 20:41:46 +0530

    more questions on async js

--- JS_Tricky_Questions/4_AsyncJS.js
+++ JS_Tricky_Questions/4_AsyncJS.js
@@ -18,6 +18,21 @@ console.log('End');
 | queueMicrotask   | setInterval     |
 | MutationObserver | I/O events      |
 
+-> "JavaScript executes synchronous code first, then microtasks like Promises, and finally macrotasks like setTimeout."
+
+Q: Why do Promises execute before setTimeout?
+A: Because microtasks have higher priority than tasks/macro task.
+
+->WHAT GOES WHERE?
+| Type         | Queue      |
+| ------------ | ---------- |
+| sync code    | Call Stack |
+| Promise.then | Microtask  |
+| await        | Microtask  |
+| setTimeout   | Task       |
+| setInterval  | Task       |
+| DOM events   | Task       |
+  - task = macro task/callback queue
 */
 console.log("**************")
 //2. What’s wrong?
@@ -36,3 +51,40 @@ Because:
 */
 //this will work:
 f().then(res => console.log("res",res)); 
+
+//3. await is async pause
+async function test() {
+  console.log("1");
+  await Promise.resolve();
+  console.log("2");
+}
+
+test();
+console.log("3");
+// o/p: 1,3,2
+//await pauses and puts remaining code into microtask queue.
+
+//4. Multiple awaits run order
+async function testTwo() {
+  console.log("A");
+  await null;
+  console.log("B");
+  await null;
+  console.log("C");
+}
+testTwo();
+// o/p: A,B,C
+//Each await waits for microtask cycle.
+
+//5. Promise without resolve
+new Promise(() => console.log("A"));
+console.log("B");
+// o/p: A,B
+//Promise constructor runs immediately.
+
+//6. setTimeout inside Promise
+Promise.resolve().then(() => {
+  setTimeout(() => console.log("New Timeout"), 0);
+});
+Promise.resolve().then(() => console.log("New Promise"));
+// o/p : New Timeout, New Promise
