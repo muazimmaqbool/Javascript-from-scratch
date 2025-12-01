24f72a98d3ec9b4ad33383816827a15557a25356
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-12-01 18:45:22 +0530

    this on arrow functions and interview questions

--- JS_Tricky_Questions/6_This.js
+++ JS_Tricky_Questions/6_This.js
@@ -3,6 +3,9 @@
 /*
 ->'this' refers to the object that is calling the function.
   It is decided by how a function is called, not where it is written.
+  or
+the this keyword is a special identifier that refers to the current execution context. 
+Its value is dynamic and depends on how and where a function is called, rather than where it is defined.
 */
 
 //1. what is the ouput of this code?
@@ -41,4 +44,38 @@ console.log(h()) // Muazim
 Why does this work?
     bind() forces this to always be user obj:
     “No matter how you call this function, this will be user obj.”
+*/
+
+/*
+->Arrow vs Regular Functions:
+| Feature           | Arrow Function  | Regular Function  |
+| ----------------- | --------------  | ----------------  |
+| `this`            | Inherited       | Dynamic           |
+| Constructor       | ❌ No           | ✅ Yes            |
+| `arguments`       | ❌ None         | ✅ Yes            |
+| `call/apply/bind` | ❌ Ignored      | ✅ Works          |
+
+*/
+
+//2. Arrow Function this
+const arrow=()=>console.log(this);
+arrow() // o/p: window (or {} in Node)
+/*
+Why?
+Arrow functions do not have their own this.
+They take this from where they are written, not called.
+If written globally:
+Browser → window
+Node → {}
+Module → undefined
+*/
+
+/*
+Final Interview Summary Lines:
+    ✅ this depends on how function is called
+    ✅ Detached method loses this
+    ✅ bind() locks this permanently
+    ✅ Arrow functions inherit this
+    ✅ var → undefined when hoisted
+    ✅ let/const → TDZ error
 */
