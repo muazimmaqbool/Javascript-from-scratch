c9ed1f9b7ced4cdd2de575849e95a13d341b597e
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-12-02 22:21:20 +0530

    debounce

--- JS_Coding_Questions/8_For_Interview.js
+++ JS_Coding_Questions/8_For_Interview.js
@@ -16,14 +16,26 @@ console.log(counter())
 console.log(counter())
 console.log(counter())
 
-//Debounce (Very common!) (important)
+//Debounce (Very common!) (important) 
+// debouce means calling a function only after the user has stopped triggering it for some time. (It delays execution until there’s a pause)
+/*
+Without debounce → API called on every keypress
+With debounce → API called only when typing stops
+*/
 //2. implement debounce
 const debounce=(fn, delay)=>{
     let timer;
     return (...args)=>{
-        clearTimeout(timer)
-        timer=setTimeout(() => {
+        clearTimeout(timer) //if a previous timer exists cancel it, it prevents function from calling too early
+        //If user triggers again → old timer cleared → new delay starts.
+        timer=setTimeout(() => { 
             fn.apply(this,args)
         }, delay);
     }
 }
+/*
+->apply in js allows you to call a function with a specified this value and arguments
+apply(this, args)
+Preserves: original this and original arguments
+So debounce does not break function context
+*/
