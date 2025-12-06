0348c39cfb67773f9c06e43c75bb5447a809e97e
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-12-06 12:50:15 +0530

    pagination

--- JS_Coding_Questions/9_ArrayCodingQuestions.js
+++ JS_Coding_Questions/9_ArrayCodingQuestions.js
@@ -2,7 +2,7 @@
 
 console.log("--------- Remove Duplicate -----------");
 //1. Remove duplicates:
-const nums = [1, 2, 3, 4, 4, 5,5,5];
+const nums = [1, 2, 3, 4, 4, 5, 5, 5];
 const unique = [...new Set(nums)];
 console.log("unique nums:", unique); // [1,2,3,4]
 /*
@@ -43,8 +43,7 @@ const removeDuplicateElement = (arr) => {
   }
   return result;
 };
-console.log("remove duplicate element:",removeDuplicateElement(nums))
-
+console.log("remove duplicate element:", removeDuplicateElement(nums));
 
 console.log("----------- Flatten Array ----------------");
 //2. Flatten Deep Array:
@@ -76,62 +75,79 @@ Explanation:
 */
 
 console.log("------- Max and Min ----------------");
-const newNums=[1,2,5,9,6,8,11]
-const maxEl=(arr)=>{
-    let max=1;
-    for(let i=0;i<arr.length;i++){
-        if(arr[i]>max){
-            max=arr[i]
-        }
+const newNums = [1, 2, 5, 9, 6, 8, 11];
+const maxEl = (arr) => {
+  let max = 1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
     }
-    return max
-}
-const minEl=(arr)=>{
-    let min=1;
-    for(let i=0;i<arr.length;i++){
-        if(arr[i]<min){
-            min=arr[i]
-        }
+  }
+  return max;
+};
+const minEl = (arr) => {
+  let min = 1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
     }
-    return min
-}
-console.log("Max Element:",maxEl(newNums))
-console.log("Min Element:",minEl(newNums))
+  }
+  return min;
+};
+console.log("Max Element:", maxEl(newNums));
+console.log("Min Element:", minEl(newNums));
 
-console.log("-------------- Rotate Array ------------")
-const arrNums=[1,2,3,4,5,6,7,8,9]
-const rotateArray=(arr)=>{
-    let newArray=[]
-    for(let i=arr.length-1;i>=0;i--){
-        // console.log(i)
-        newArray.push(arr[i])
-    }
-    return newArray;
-}
-console.log("Rotate Array:",rotateArray(arrNums))// o/p: [9,8,7,6,5,4,3,2,1]
+console.log("-------------- Rotate Array ------------");
+const arrNums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const rotateArray = (arr) => {
+  let newArray = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    // console.log(i)
+    newArray.push(arr[i]);
+  }
+  return newArray;
+};
+console.log("Rotate Array:", rotateArray(arrNums)); // o/p: [9,8,7,6,5,4,3,2,1]
 
-console.log("--------- Check array if sorted -------------")
-const isSorted=(arr)=>{
-  for(let i=0;i<arr.length;i++){
+console.log("--------- Check array if sorted -------------");
+const isSorted = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
     // console.log("arr[i]",arr[i])
     // console.log("arr[i-1]",arr[i-1])
-    if(arr[i]<arr[i-1]) return false
+    if (arr[i] < arr[i - 1]) return false;
   }
-  return true
-}
-console.log("isSorted:",isSorted([1,2,5,3,6,8])) //false
-console.log("isSorted:",isSorted([1,2,3,4,5])) //true
+  return true;
+};
+console.log("isSorted:", isSorted([1, 2, 5, 3, 6, 8])); //false
+console.log("isSorted:", isSorted([1, 2, 3, 4, 5])); //true
 
-console.log("--------- Count Occurences ------------")
-const countOccurences=(arr,element)=>{
+console.log("--------- Count Occurences ------------");
+const countOccurences = (arr, element) => {
   //checking how many times the element has occured in the array
-  let count=0;
-  for(let i=0;i<arr.length;i++){
-    if(arr[i]===element){
-      count++
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === element) {
+      count++;
     }
   }
-  return count
-}
-console.log("countOccuences:",countOccurences([1,2,3,4,5,5,6,6,6],6)) //3
-console.log("countOccuences:",countOccurences(['A','B','C','B'],'B')) //2
+  return count;
+};
+console.log("countOccuences:", countOccurences([1, 2, 3, 4, 5, 5, 6, 6, 6], 6)); //3
+console.log("countOccuences:", countOccurences(["A", "B", "C", "B"], "B")); //2
+
+console.log("------------- pagination -------------");
+//split array into chunks
+//The goal of this function is to break a large array into smaller pieces (pages or chunks) of fixed size
+const pagination = (arr, size) => {
+  let res = [];
+  //So if size = 3, i becomes: 0 → 3 → 6 → 9
+  for (let i = 0; i < arr.length; i += size) {
+    //slice: cuts part of the array
+    //arr.slice(i,i+size): extracts a piece of the array starting at i and ending at i + size
+    //eg: arr.slice(0, 3) // elements from index 0 to 2
+    res.push(arr.slice(i, i + size));
+  }
+  return res;
+};
+console.log("pagination:",pagination([1,2,3,4,5,6,7,8,9,10],3))
+//o/p: [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ], [ 10 ] ]
