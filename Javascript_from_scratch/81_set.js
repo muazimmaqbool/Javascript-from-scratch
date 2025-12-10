8fb13710caa29a3850d994fb80a7934d51201060
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-12-10 19:05:05 +0530

    set

--- Javascript_from_scratch/81_set.js
+++ Javascript_from_scratch/81_set.js
@@ -1,6 +1,6 @@
-//Command to run this code: node 81_set.js
+//Command to run this code: node --watch 81_set.js
 
-//Set(): In JavaScript, a Set is a built-in object that lets you store unique values of any type,
+//Set(): In JavaScript, a Set is a built-in object that let's you store unique values of any type,
           //This means that each value can only appear once within a Set.
 
 //Example:
@@ -23,35 +23,36 @@ fruits.add("Apple");
 fruits.add("Orange");
 fruits.add("Banana");
 fruits.add("Apple"); // ignored as its duplicate
-console.log("Fruits:",fruits) // o/p : { 'Apple', 'Orange', 'Banana' }
+console.log("Fruits set:",fruits) // o/p : { 'Apple', 'Orange', 'Banana' }
 
-console.log("size:",fruits.size)
+console.log("fruits set size:",fruits.size) // 3
 console.log("has apple:",fruits.has("Apple")) // true
-console.log('has mango:',fruits.has("mango")) // mango
+console.log('has mango:',fruits.has("mango")) // false
+console.log("has banana:",fruits.has("banana")) // fase (because b is small)
 
 console.log("deleting banana...")
 fruits.delete("Banana")
-console.log("fruits:",fruits)
+console.log("fruits set afte deletion:",fruits)
 console.log("clear all")
 
-// fruits.clear()
-//console.log("fruits:",fruits) // o/p: Set(0) {} //checkout this after fruits.clear()
+fruits.clear()
+console.log("fruits cleared:",fruits) // o/p: Set(0) {} //checkout this after fruits.clear()
 
 //->Looping Through a Set
 fruits.add("mango")
 fruits.add("watermellon")
 fruits.add("grapes")
 for(const i of fruits){
-    console.log(i)
+    console.log("i = ",i)
 }
 //or
 fruits.forEach((val)=>console.log(val))
 
 //->Convert Between Set and Array
 //set to array
-console.log("fruits:",fruits) // o/p: Set(5) { 'Apple', 'Orange', 'mango', 'watermellon', 'grapes' }
-const arr=[...fruits] // or Array.from(fruits)
-console.log("arr:",arr) // o/p: [ 'Apple', 'Orange', 'mango', 'watermellon', 'grapes' ]
+console.log("fruits:",fruits) // o/p: Set(3) {'mango', 'watermellon', 'grapes' }
+const arr=[...fruits] // or const arr = Array.from(fruits)
+console.log("arr:",arr) // o/p:  ['mango', 'watermellon', 'grapes']
 //Array to set
 const uniqueArr=new Set(['a','b','a','c']) //it will also remove duplicate
 console.log("unique arr:",uniqueArr) // o/p: Set(3) { 'a', 'b', 'c' }
