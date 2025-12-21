2fe00b05c00477a25286219b22f4f90faf78689b
Muazim Maqbool <muazimmaqbool4@gmail.com>
2025-12-21 17:23:56 +0530

    once function explanation

--- JS_Coding_Questions/11_OnceFunc.js
+++ JS_Coding_Questions/11_OnceFunc.js
@@ -12,28 +12,28 @@ Problem Statement:
 
 // once is a higher-order function that accepts a function as an argument
 const once=(fn)=>{ 
-    // This variable keeps track of whether the function has already run
-    // It is stored in the closure and persists between calls
+    // keeps track of whether the function has already run, it is stored in the closure and persists between calls
     let hasRun=false;
-    // once() returns a new function
-    // This returned function is what we will actually call
+    // once() returns a new function, this returned function is what we will actually call
     return function(){
         if(!hasRun){
-            // Call the original function for the first time
+            // calls the original function for the first time when hasRun is false then updates it to true
             fn()
             hasRun=true
         }
     }
 }
-// Function that we want to execute only once
+
+//function that we want to execute only once
 const fn = () => console.log("Initialized");
-// Wrap fn using once()
-// initialize now holds the returned function with a closure over hasRun
+
+//initialize now holds the returned function with a closure over hasRun
 const initialize=once(fn)
 // console.log("intializeL",initialize) 
-initialize(); // First call → hasRun is false → fn() executes
-initialize();// Second call → hasRun is true → fn() is skipped
-initialize();// Third call → still true → nothing happens
+
+initialize(); // First call → hasRun is false → fn() executes -> hasRun becomes true
+initialize(); // Second call → hasRun is true → fn() is skipped
+initialize(); // Third call → still true → nothing happens
 
 /*
 o/p: Initialized
