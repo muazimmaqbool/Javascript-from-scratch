f5cd24a1de5b511555fef258ee9c31de25e96400
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-01-05 21:41:50 +0530

    subarrays of array

--- DSA/2_CoreDataStructures/2_Array.js
+++ DSA/2_CoreDataStructures/2_Array.js
@@ -1,8 +1,8 @@
 //To run go inside this folder via cd and enter: node --watch 2_Array.js (--watch is optional)
 
-//Q: sub-arrays all possible:
+//Q: find sub-arrays all possible:
 //This function is designed to return only contiguous subarrays (i.e. elements next to each other in the original array)
-// that means [1,3] will not be returned
+//that means [1,3] will not be returned
 function subarrays(arr) {
   let result = [];
   for (let i = 0; i < arr.length; i++) {
