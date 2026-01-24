e06a0f1fc837a2c3a3aa18d031624270d4ecd746
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-01-24 22:03:58 +0530

    object and arrays

--- JS_Coding_Questions/5_Objects_Arrays.js
+++ JS_Coding_Questions/5_Objects_Arrays.js
@@ -13,14 +13,14 @@ INTERVIEW ONE-LINERS (IMPORTANT)
 ✅ Object.entries() is great for looping
 */
 
-//Note-> Learn shallow and deep copy from javascript_from_scratch folder code number 71
+//Note-> Learn shallow and deep copy from 'javascript_from_scratch' folder 'code number 71'
 
 //1. What’s the output?
 const obj1 = { a: 1 };
 const obj2 = obj1;
 // console.log("obj2:",obj2)
 obj2.a = 2;
-console.log(obj1.a);
+console.log("obj1.a",obj1.a);
 /*
 Objects are stored by reference, not by value.
 obj2 = obj1 → both point to the SAME object in memory
@@ -32,7 +32,7 @@ In JavaScript, assigning an object copies the reference, not the object.
 //2. What is the output?
 const arr = [1, 2, 3];
 arr.length = 0;
-console.log(arr); // o/p: []
+console.log("arr",arr); // o/p: []
 
 //3. What is the output?
 const obj={ a: 1, b: 2 }
