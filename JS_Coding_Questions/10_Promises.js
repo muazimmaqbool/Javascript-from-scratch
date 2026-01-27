22d04aebe75f408ce8a60e983346fd1439b25951
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-01-27 21:45:43 +0530

    fn call with retry

--- JS_Coding_Questions/10_Promises.js
+++ JS_Coding_Questions/10_Promises.js
@@ -7,12 +7,12 @@ async function retry(fn, retries = 3) {
   } catch (err) {
     if (retries === 0) throw err;
     console.log("retry:", retries);
-    //Recursive retry with reduced count.
+    //Recursive retry function call with reduced count.
     return retry(fn, retries - 1);
   }
 }
 //It retries a failed async function automatically.
-retry(() => fetch("data/api/")) // replace api inside fetch with this: https://jsonplaceholder.typicode.com/posts, it returns success
+retry(() => fetch("data/api/")) // replace api inside fetch with this: https://jsonplaceholder.typicode.com/posts : it returns success
   .then((data) => console.log("Success"))
   .catch((err) => console.log("Failed after retries"));
 
