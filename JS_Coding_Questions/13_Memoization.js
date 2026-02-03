a1742a215e3f9641a2916ff2b1900fd3d6bde29b
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-02-03 19:21:40 +0530

    memoization info

---
+++ JS_Coding_Questions/13_Memoization.js
@@ -0,0 +1,12 @@
+/*
+->Memoziation:
+    Memoization is an optimization technique used to speed up function calls by caching the results of expensive function calls
+    and returning the cached result when the same inputs occur again.
+
+    Implement a memoization function that takes a function as an argument and returns a memoized version of that function. 
+    The memoized function should cache the results based on the arguments passed to it.
+
+->What is Memoization (in simple words)?
+    Memoization = "Don’t repeat expensive work if you already did it once."
+    If a function is called again with the same inputs, return the stored result instead of recalculating.
+*/
