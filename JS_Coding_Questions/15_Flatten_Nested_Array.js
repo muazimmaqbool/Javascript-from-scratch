2433a59e6561e9b7e12c507cc42a34a45d82c4b5
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-02-13 16:38:43 +0530

    flatten array solution two

--- JS_Coding_Questions/15_Flatten_Nested_Array.js
+++ JS_Coding_Questions/15_Flatten_Nested_Array.js
@@ -4,9 +4,30 @@ Example:
     input=[1,[2,[3,4]5],6]
     ouput=[1,2,3,4,5,6]
 */
+
 //Solution One:
 const flattenArrayOne=(arr)=>{
     return arr.flat(Infinity)
+}
+console.log(flattenArrayOne([1, [2, [3, 4], 5], 6]))
+//o/p: [1,2,3,4,5,6]
+console.log(flattenArrayOne(['a','b',['c','d'],['e',['f','g']],'i']))
+//o/p: ['a', 'b', 'c','d', 'e', 'f','g', 'i']
+
+console.log("*****************")
+//Solution Two:
+const flattenArrayTwo=(arr)=>{
+    const result=[]
+    for(let i=0;i<arr.length;i++){
+        if(Array.isArray(arr[i])){
+            result=result.concat(flattenArrayTwo(arr[i]))
+            //or
+            //result=result.push(flattenArrayTwo(arr[i]))
+        }else{
+            result.push(arr[i])
+        }
+    }
+    return result
 }
 console.log(flattenArrayOne([1, [2, [3, 4], 5], 6]))
 //o/p: [1,2,3,4,5,6]
