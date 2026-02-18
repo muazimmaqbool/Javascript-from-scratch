ce7c3179dda666923cb038fe0e93c4700d068723
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-02-18 23:07:43 +0530

    what sort does

--- JS_Coding_Questions/16_Sort_Compare_Working.js
+++ JS_Coding_Questions/16_Sort_Compare_Working.js
@@ -1,1 +1,8 @@
 //This code explain the working of sort() when used to compare two values:
+//sort((a, b) => (b + a).localeCompare(a + b))
+
+//1. What sort normally does:
+/*
+If you do: ["9", "34", "5"].sort()
+You get: ["34", "5", "9"], this is not what you want, Because default sort is alphabetical (string).
+*/
