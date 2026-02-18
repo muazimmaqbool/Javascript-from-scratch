241a56bafe77790eda6b3be66ffe2ca316a058f1
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-02-18 23:08:58 +0530

    comparator role

--- JS_Coding_Questions/16_Sort_Compare_Working.js
+++ JS_Coding_Questions/16_Sort_Compare_Working.js
@@ -6,3 +6,9 @@
 If you do: ["9", "34", "5"].sort()
 You get: ["34", "5", "9"], this is not what you want, Because default sort is alphabetical (string).
 */
+
+//2. What this comparator is saying:
+/*
+The function: (a, b) => (b + a).localeCompare(a + b)
+Means: "To decide the order of a and b, compare which is bigger: b followed by a OR a followed by b."
+*/
