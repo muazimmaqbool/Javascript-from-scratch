ac50d2cb67426189ee638c23748b6582ed7dcb85
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-02-20 19:31:22 +0530

    example of shuffle

--- JS_Coding_Questions/17_Shuffle.js
+++ JS_Coding_Questions/17_Shuffle.js
@@ -4,13 +4,14 @@
 input-[1,2,3,4,5]
 output=[4,2,5,1,3], etc
 */
-function shuffle(arr){
+function shuffle(array){
     const result = array.slice()
-    console.log(result)
+    // console.log(result)
     for (let i=result.length - 1; i > 0;i--){
         const j = Math.floor(Math.random() * (i + 1));
         [result[i], result[j]] = [result[j], result[i]];
     }
     return result
 }
-console.log()
+console.log(shuffle([1,2,3,4,5])) // [ 3, 2, 5, 4, 1 ]  (different o/p everytime)
+console.log(shuffle(['a','b','c','d'])) // [ 'd', 'b', 'a', 'c' ] (different o/p everytime)
