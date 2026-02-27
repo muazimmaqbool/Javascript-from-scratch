41ce49de6f9ce4263408d1674b56dac8487f4bb7
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-02-27 11:42:18 +0530

    range of numbers explanied

--- DSA/4_Recursion/2_Questions_On_Recursion.js
+++ DSA/4_Recursion/2_Questions_On_Recursion.js
@@ -14,6 +14,7 @@ console.log("Create array with range of number:")
 //2: Create an array with range of numbers.
 //input: start=1, end=5 : output: [1,2,3,4,5]
 function rangeOfNumbers(startNum,endNum){
+    // console.log("startNum:",startNum,": endNum:",endNum)
     if(endNum<startNum){
         return []
     }
@@ -25,3 +26,27 @@ function rangeOfNumbers(startNum,endNum){
 }
 console.log(rangeOfNumbers(1,5)) // [1,2,3,4,5]
 console.log(rangeOfNumbers(-1,3)) // [-1,0,1,2,3]
+/*
+Explanation:
+Core Idea:
+    Keeps reducing endNum
+    Goes all the way down until base case
+    Then builds array while coming back up (unwinding)
+
+Call 1: rangeOfNumbers(1,5), it calls: rangeOfNumbers(1,4)
+Call 2: rangeOfNumbers(1,4), it calls: rangeOfNumbers(1,3)
+Call 3: rangeOfNumbers(1,3), it calls: rangeOfNumbers(1,2)
+Call 4: rangeOfNumbers(1,2), it calls: rangeOfNumbers(1,1)
+Call 5: rangeOfNumbers(1,1), it calls: rangeOfNumbers(1,0) now endNum is < startNum i.e 0 < 1 so it returns []
+
+->Now Stack Unwinds (This Is Important)
+Now each function call resumes and pushes its endNum.
+start filling endNum from bottom
+->rangeOfNumbers(1,5) => [1,2,3,4,5]
+->rangeOfNumbers(1,4) => [1,2,3,4]
+->rangeOfNumbers(1,3) => [1,2,3]
+->rangeOfNumbers(1,2) => [1,2]
+->rangeOfNumbers(1,1) => [1]
+->rangeOfNumbers(1,0) => []
+
+*/
