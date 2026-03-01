6fd27f9bfac9696a24da8b100523ffd215f10591
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-03-01 22:08:03 +0530

    reverse a number using recursion

--- DSA/4_Recursion/3_More_Questions.js
+++ DSA/4_Recursion/3_More_Questions.js
@@ -1,10 +1,23 @@
 //More question on recursion.
 
+console.log("Q: Reverse a string:")
 //Q: Reverse a string
 //Example: "Hello" ->>> "olleH", "react" ->>> "tcaer"
 
+//Solution one without recursion
 function reverseStr(str){
     return str.split("").reverse().join("")
 }
 console.log("Hello :",reverseStr("Hello")) // olleh
 console.log("react :",reverseStr("react")) // tcaer
+
+//Solution two using recursion
+function reverseUsingRecursion(str){
+    if(str===""){
+        return ""
+    }else return reverseUsingRecursion(str.substr(1))+str.charAt(0)
+}
+console.log("Java :",reverseUsingRecursion("Java")) // avaJ
+console.log("phone :",reverseUsingRecursion("phone")) // enohp
+
+//Explanation:
