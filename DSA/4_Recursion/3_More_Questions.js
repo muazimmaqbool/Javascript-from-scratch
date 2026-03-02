783db84af36290df1cc2c878a28980193138785d
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-03-02 21:53:04 +0530

    reverse a string using recursion explained

--- DSA/4_Recursion/3_More_Questions.js
+++ DSA/4_Recursion/3_More_Questions.js
@@ -21,3 +21,18 @@ console.log("Java :",reverseUsingRecursion("Java")) // avaJ
 console.log("phone :",reverseUsingRecursion("phone")) // enohp
 
 //Explanation:
+/*
+->Input "java":
+  first iteration: reverseUsingRecursion("Java") => reverseUsingRecursion("ava") (calls with str which starts from index 1) + j (i.e adding first character of str which is j)
+  second iteration: reverseUsingRecursion("ava") => reverseUsingRecursion("va") + a 
+  third iteration: reverseUsingRecursion("va") => reverseUsingRecursion("a") + v
+  fourth iteration: reverseUsingRecursion("a") => reverseUsingRecursion("") + a
+  fith iteration: reverseUsingRecursion("")=> base case it return ""
+  
+  ->Now see what they return from bottom (i.e from 5th to first iteration):
+  first iteration: reverseUsingRecursion("Java") => reverseUsingRecursion("ava") + j => avaj ( returns avaj i.e the output)
+  second iteration: reverseUsingRecursion("ava") => reverseUsingRecursion("va") + a => av+a => ava (becasue reverseUsingRecursion("va") returns us av in third iteration)
+  third iteration: reverseUsingRecursion("va") => reverseUsingRecursion("a") + v => a+v => av (becasue reverseUsingRecursion("a") returns us a in fourth iteration)
+  fourth iteration: reverseUsingRecursion("a") => everseUsingRecursion("") + a =>""+ a => a ((becasue reverseUsingRecursion("") returns us "" in fifth iteration))
+  fith iteration: reverseUsingRecursion("")=> base case it return ""
+*/
