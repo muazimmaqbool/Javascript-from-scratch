030a5e3cd84ddcfc5dfebe3c5e5270bfab51d545
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-03-02 22:00:00 +0530

    sub sets question

---
+++ DSA/4_Recursion/4_Subsets.js
@@ -0,0 +1,11 @@
+/*
+->Important question
+
+Q: Subets (Backtracking algorithm using recursion)
+
+->Given an integer array nums of unique elements, return all possible subsets (the power set).
+ The solution set must not contain duplicate subsets. Return the solution in any order.
+
+ input= [1,2,3] ->>> output= [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+ input= [0] ->>> output= [[],[0]]
+*/
