023a349f44e25e67e08506219cca4ab8fd9bd63e
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-03-03 22:11:24 +0530

    stack implementation in js

--- DSA/5_Stack/1_Intro.js
+++ DSA/5_Stack/1_Intro.js
@@ -13,4 +13,59 @@
     2. pop(): Removes and returns the top element from the stack. If the stack is empty, it returns undefined (or an error in some implementations).
     3. peek() or top(): Returns the value of the top element without removing it from the stack.
     4. isEmpty(): A helper method that checks if the stack contains any elements, returning a boolean value.
+    5. size() : Returns the size of the stack (array)
 */
+
+//Basic Stack Implementation:
+class Stack {
+  //Class is basically a template that is used to create objects in js
+
+  //when we create the new instance of the stack class this constructor will be the one to run first
+  constructor() {
+    this.stack = []; //declaring a local variable stack with initial value as empty array []
+    //so whenever we initialize new object of class stack the will create a stack variable for that object via the constructor
+  }
+
+  //adding element to the stack:
+  push(el) {
+    this.stack.push(el);
+  }
+  //removing top element of the stack
+  pop() {
+    if (this.isEmpty()) {
+      return "Stack is empty!";
+    }
+    return this.stack.pop();
+  }
+
+  isEmpty() {
+    // return this.stack.length===0 ? true : false
+    return this.stack.length === 0;
+  }
+
+  //returning top element of the stack
+  peek() {
+    if (this.isEmpty()) {
+      return "Stack is empty!";
+    }
+    return this.stack[this.stack.length - 1];
+  }
+  size() {
+    return this.stack.length;
+  }
+}
+
+const stackObj=new Stack(); //stackObj is the object of the class, now this stackObj can access all its function
+
+console.log(stackObj.isEmpty())// true
+console.log(stackObj.peek())// Stack is empty!
+stackObj.push(1);
+stackObj.push(2)
+stackObj.push(3)
+stackObj.push(4)
+console.log(stackObj.isEmpty()) // false
+console.log(stackObj.peek()) // 4
+console.log(stackObj.pop()) // 4
+console.log(stackObj.peek()) // 3
+stackObj.pop()
+console.log(stackObj.size()) // 2
