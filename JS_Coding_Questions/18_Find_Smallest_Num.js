7f1cf5a51077173a9c0af87556722174a253a370
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-03-03 19:58:29 +0530

    smallest example

--- JS_Coding_Questions/18_Find_Smallest_Num.js
+++ JS_Coding_Questions/18_Find_Smallest_Num.js
@@ -13,9 +13,14 @@ function getSmallest(arr){
         let el=arr[i]
         //checking if element is not a number or if its infinity or -infinity
         if(typeof el!=="number" || !Number.isFinite(el)) return false
-        if(smallest===undefined || smallest<el){
+        if(smallest===undefined || el<smallest){
             smallest=el
         }
     }
     return smallest
 }
+console.log(getSmallest([])) // null
+console.log(getSmallest(123)) // false
+
+console.log(getSmallest([1,2,5,6,3,2])) // 1
+console.log(getSmallest([5,8,66,0,-5])) // -5
