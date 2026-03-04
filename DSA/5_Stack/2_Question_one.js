b7debe5d6bc3c9c824c7b50e74a09ff7aee5ce03
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-03-04 21:56:30 +0530

    question one setup

---
+++ DSA/5_Stack/2_Question_one.js
@@ -0,0 +1,5 @@
+//Question 1: Given an input string s, reverse the order of the words
+
+//input: "the sky is blue" ->>> output: "blue is sky the"
+//input: "hello world" ->>> output: "world hello"
+//input: "a good boy" ->>> output: "boy good a "
