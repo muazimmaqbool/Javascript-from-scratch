bc2be28a530d81260d6179f1abbd6b4e13381032
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-03-05 21:58:42 +0530

    time and space complexity

--- DSA/5_Stack/2_Question_one.js
+++ DSA/5_Stack/2_Question_one.js
@@ -1,4 +1,4 @@
-//Question 1: Given an input string s, reverse the order of the words
+//Question 1: Given an input string s, reverse the order of the words (Using Stack)
 
 //input: "the sky is blue" ->>> output: "blue is sky the"
 //input: "  hello world    " ->>> output: "world hello"
@@ -32,3 +32,10 @@ const reverseWords=(s)=>{
 console.log(reverseWords("the sky is blue")) // blue is key the
 console.log(reverseWords("       hello world  ")) // world hello
 console.log(reverseWords("a good                boy")) //  boy good a 
+
+//for-loop O(n) and while-loop O(n)
+//Time complexity : O(2n) here we don't take constant so : O(n)
+
+//Space complexity : O(n)
+
+//Note this questions as a better approach but here we are doing it using strings
