d8626eb286144fac99e4eae6a041e05a04b039ce
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-03-05 19:30:12 +0530

    solution two using reduce

--- JS_Coding_Questions/21_Sum.js
+++ JS_Coding_Questions/21_Sum.js
@@ -3,8 +3,8 @@
 //input : sum() => 0
 //input : sum(5,-5,10,20)=30
 
+//solution 1:
 function sum(...args){
-    //solution 1:
     let sum=0;
     for(let i=0;i<args.length;i++){
         sum+=args[i]
@@ -14,3 +14,13 @@ function sum(...args){
 console.log(sum(1,2,3)); // 6
 console.log(sum()) // 0
 console.log( sum(5,-5,10,20)) // 30
+
+console.log("***************")
+//solution 2:
+function sumUsingReduce(...args){
+    return args.reduce((acc,curr)=>acc+curr,0)
+}
+console.log(sumUsingReduce(1,2,3)); // 6
+console.log(sumUsingReduce()) // 0
+console.log( sumUsingReduce(5,-5,10,20)) // 30
+console.log(sumUsingReduce(10))  // 10
