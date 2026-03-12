3d20c0db3a6f97db4ffb7646b55f518cb0532d29
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-03-12 17:32:33 +0530

    question setup

---
+++ DSA/6_Linear_AND_Binary_Search/4_Count_Pos_Neg.js
@@ -0,0 +1,17 @@
+/*
+Ques 4: Maximum Count of Positive Integer and Negative Integer
+    Given an array nums sorted in non-decreasing order, return the maximum between
+    the number of positive integers and the number of negative integers.
+
+Input: nums = [-2,-1,-1,1,2,3] ---> Output: 3
+        Explanation: There are 3 positive integers and 3 negative integers.
+        The maximum count among them is 3.
+
+Input: nums = [-3,-2,-1,0,0,2,3] ---> Output: 3
+        Explanation: There are 2 positive integers and 3 negative integers.
+        The maximum count among them is 3.
+
+Input: nums = [10,20,30,40] ---> Output: 4
+        Explanation: There are 4 positive integers and 0 negative integers.
+        The maximum count among them is 4.
+*/
