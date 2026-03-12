0b1fb83f56aa57311230e9c2b2d29780ded14e8b
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-03-12 17:05:45 +0530

    kth question setup

---
+++ DSA/6_Linear_AND_Binary_Search/3_Kth_Missing_Number.js
@@ -0,0 +1,13 @@
+//Imporatnt:
+/*
+Ques 3: Kth Missing Positive Number
+Given an array arr of positive integers sorted in a strictly increasing order,
+and an integer k. Return the kth positive integer that is missing from this array.
+
+Input: arr = [2,3,4,7,11], k = 5 ---------- > output= 9
+Explanation: The missing positive integers are [1,5,6,8,9,10,12,13, ... ].
+The 5th missing positive integer is 9.
+*/
+
+//Solution: (We will be using linear search to solve this question)
+function 
