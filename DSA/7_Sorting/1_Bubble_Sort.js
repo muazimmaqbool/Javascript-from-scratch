187ffcc69ed7e300b98ee2df1eb728e141f7fd11
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-03-13 22:14:34 +0530

    code for bubble sort

--- DSA/7_Sorting/1_Bubble_Sort.js
+++ DSA/7_Sorting/1_Bubble_Sort.js
@@ -26,3 +26,21 @@
         nested loops, and algorithm analysis. 
         It can be suitable for very small datasets or data that is already nearly sorted
 */
+//Question: Implement bubble sort in js.
+//Write a function to sort the given array nums in ascending order.
+
+//Input= [29,10,14,37,14] ---> output= [10,14,14,29,37]
+
+//Solution:
+const bubbleSort = (arr) => {
+  const n = arr.length;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+};
+console.log(bubbleSort([29, 10, 14, 37, 14])); // [10,14,14,29,37]
