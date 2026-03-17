0b3c57baf6e07e22f838bcfe27a0f8814e26389d
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-03-17 14:24:35 +0530

    code for insertion sort

--- DSA/7_Sorting/3_Insertion_Sort.js
+++ DSA/7_Sorting/3_Insertion_Sort.js
@@ -14,6 +14,25 @@
     5)Insert the key: Continue shifting until you find a position where an element is smaller than or equal to the key, then insert the key into the newly created space.
     6)Repeat: Move to the next unsorted element and repeat the process until the entire array is sorted
 
+->How it Works (Step-by-Step): (in simple terms)
+    1) Start with first element as sorted
+      👉 Assume first number is already in correct place
+
+    2) Pick the next element (key)
+      👉 Take next number from unsorted part
+
+    3) Compare with left side
+      👉 Check numbers before it (sorted part)
+
+    4) Shift bigger elements to the right
+      👉 If any number is bigger than key, move it one step right
+
+    5) Insert the key
+      👉 Place the key in its correct position
+
+    6) Repeat
+      👉 Do the same for all elements until array is sorted
+
 ->Complexity Analysis:
   1)Best Case: O(n). This occurs when the array is already sorted. The algorithm makes only one comparison per element, and no shifting is required.
   2)Average Case: O(n²). This is the typical performance for a randomly ordered list.
@@ -28,3 +47,24 @@
    4)Stable: It maintains the relative order of elements that have equal values.
    5)Online Sorting: It can sort a list as it receives elements incrementally.
 */
+//Note: Visit -> https://visualgo.net/en/sorting to see visually working of different sort algorithms
+
+//Solution:
+//Q: Implement Insertion sor in js.
+//   Write a function to sort the given array nums in ascending order.
+//   Input: [29,10,14,37,14,33,8,11] --> output: [8,10,11,14,14,29,33,37]
+
+function insertionSort(arr) {
+  const n = arr.length;
+  for (let i = 1; i < n; i++) {
+    const key = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key;
+  }
+  return arr;
+}
+console.log(insertionSort([29, 10, 14, 37, 14, 33, 8, 11])); // [8,10,11,14,14,29,33,37]
