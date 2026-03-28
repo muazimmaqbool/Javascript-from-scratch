8a215c0af53b7a781369f18a36d8168028d4a50d
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-03-28 20:10:10 +0530

    singly linked list

---
+++ DSA/8_Linked_List/2_Singly_Linked_List.js
@@ -0,0 +1,19 @@
+//Note: First checkout previous file 1_intro.txt
+
+//Singly Linked List: Each node has a pointer only to the next node in the sequence. This is the simplest type.
+/*
+    
+->SINGLY LINKED LIST (Simple Diagram):
+
+    Head
+    ↓
+    [10 | • ] → [20 | • ] → [30 | • ] → [40 | NULL]
+
+    Explanation:
+        [Data | Next]
+        Data = value stored in node
+        Next = pointer/reference to next node (next is denoted by .)
+
+        Head → starting point of the list
+        Last node → points to NULL (end of list)
+*/
