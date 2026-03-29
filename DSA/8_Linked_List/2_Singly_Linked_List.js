eaa5fa39a010a3e49341c470b7957de60b0790ef
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-03-29 11:32:45 +0530

    adding element at the tail

--- DSA/8_Linked_List/2_Singly_Linked_List.js
+++ DSA/8_Linked_List/2_Singly_Linked_List.js
@@ -31,19 +31,42 @@ class Node {
 }
 
 // LinkedList class → manages the whole list
-class LinkedList{
-    constructor(){
-        this.head=null; // start of the list - initially it's empty
-    }
-    
-    // adding element at the beginning (head of the list)
-    addFirst(data){
-        const newNode=new Node(data); // create a new node with given data
-        newNode.next=this.head;  // pointing new node's next to current head
-        this.head=newNode; // update head to new node (new node becomes first element)
-    }
-    // create node → point it to current head → make it new head
+class LinkedList {
+  constructor() {
+    this.head = null; // start of the list - initially it's empty
+  }
 
+  //1) adding element at the beginning (head of the list)
+  addFirst(data) {
+    const newNode = new Node(data); // create a new node with given data
+    newNode.next = this.head; // pointing new node's next to current head
+    this.head = newNode; // update head to new node (new node becomes first element)
+  }
+  // create node → point it to current head → make it new head
+
+  //2)Adding element at then end of the list (at the tail of the linked list)
+  addLast(data) {
+    const newNode = new Node(data);
+
+    //checking if their is something in the LL or not, i.e if head is null means LL is empty
+    //then we need put it in the head position i.e as first element
+    if (this.head === null) {
+      this.head = newNode;
+      return;
+    }
+
+    //and if LL is not empty, we will traverse through the LL until we reach the node whose next is null, means we have reached the tail/end of the LL
+    let current = this.head; //current by default will be first element of the LL.
+
+    // while loop will run until current.next is not null
+    while (current.next) {
+        //everytime we go inside this loop it will assign the next node  to this current variable
+      current=current.next
+    }
+    //when we have reached end, i.e current.next is null, now we will add new node
+    current.next=newNode;
+    newNode.next=null
+  }
 }
 //Explanation:
 /*
