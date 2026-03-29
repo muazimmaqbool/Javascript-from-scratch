30e6efedb8bd574a5289da6f1513ce23a0b617b4
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-03-29 22:07:27 +0530

    comments added at addAt in DLL

--- DSA/8_Linked_List/3_Doubly_Linked_List.js
+++ DSA/8_Linked_List/3_Doubly_Linked_List.js
@@ -25,48 +25,107 @@
 // Node class → represents a single element in the list
 class Node {
   constructor(data) {
-     this.prev=null; // points to the previous node (initially null)
+    this.prev = null; // points to the previous node (initially null)
     this.data = data; // store value
     this.next = null; // pointer to next node (initially null)
   }
 }
 
-class DoublyLinkedList{
-    constructor(){
-        this.head=null; // start of the doubly linked list - initially it's empty
+class DoublyLinkedList {
+  constructor() {
+    this.head = null; // start of the doubly linked list - initially it's empty
+  }
+
+  //1)adding element at Top (Inserting something at the top of the list i.e at the head)
+  addFirst(data) {
+    const newNode = new Node(data);
+    //now newNode will point to the current head
+    newNode.next = this.head;
+
+    //if head already has something(i.e list is not empty)
+    //then connecting that node i.e (current head) to the newNode, by updating previous pointer of the current head to newNode
+    if (this.head) {
+      this.head.prev = newNode;
+    }
+    //Now updating head to newNode
+    this.head = newNode;
+  }
+
+  //2) adding element at the end i.e at the tail of the DLL
+  addLast(data) {
+    const newNode = new Node(data);
+
+    //if list is empty then adding element at the head i.e at the top/beginning
+    if (this.head === null) {
+      this.head = newNode;
+      return;
+    }
+    //If list is not empty we will start from head and traverse till end when current.next is null
+    let current = this.head;
+    while (current.next) {
+      //it will run until current.next is not null
+      current = current.next;
+    }
+    //Now after reaching tail, we will assign newNode's prev to current and current's next to new node
+    newNode.prev = current;
+    current.next = newNode;
+  }
+
+  //3) This function will return size of the LL
+  size() {
+    let count = 0;
+    let current = this.head;
+    //starting from head, we will traverse list until we react end, i.e until current is present
+    while (current) {
+      count++;
+      current = current.next;
+    }
+    return count;
+  }
+
+  //4)Adding data to the DLL at the specific index:
+  //we will insert it after the index i.e if povided index is 2 we will inex
+  addAt(index, data) {
+    if (index < 0 || index > this.size()) {
+      console.log("Invalid index provided.");
+      return;
+    }
+    const newNode = new Node(data);
+    if (index === 0) {
+      newNode.next = this.head;
+      if (this.head) {
+        this.head.prev = newNode;
+      }
+      this.head = newNode;
+      return;
     }
 
-    //1)adding element at Top (Inserting something at the top of the list i.e at the head)
-    addFirst(data){
-        const newNode=new Node(data);
-        //now newNode will point to the current head
-        newNode.next=this.head;
-
-        //if head already has something(i.e list is not empty) 
-        //then connecting that node i.e (current head) to the newNode, by updating previous pointer of the current head to newNode
-        if(this.head){
-            this.head.prev=newNode
-        }
-        //Now updating head to newNode
-        this.head=newNode
+    let current = this.head;
+    //we will go till index-1. Suppose the provided index is 3, so we will go till index 2, because index 2's next is pointing to 3 index. So we will point 2's next to newNode's next,
+    for (let i = 0; i < index-1; i++) {
+      current = current.next;
     }
+    // new node will point forward to the next node (current.next is point to next node after current)
+    newNode.next = current.next;
+    // new node will point backward to the current node
+    newNode.prev = current;
 
-    //2) adding element at the end i.e at the tail of the DLL
-    addLast(data){
-        const newNode=new Node(data)
-        
-        //if list is empty then adding element at the head i.e at the top/beginning
-        if(this.head===null){
-            this.head=newNode
-            return
-        }
-        //If list is not empty we will start from head and traverse till end when current.next is null 
-        let current=this.head;
-        while(current.next){ //it will run until current.next is not null
-            current=current.next
-        }
-        //Now after reaching tail, we will assign newNode's prev to current and current's next to new node
-        newNode.prev=current;
-        current.next=newNode
+    // if next node exists, update its prev to point to new node
+    //means current.next is not null means it's not tail we have another node after current, so we will point that node's previous to newNode
+    if(current.next){
+        current.next.prev=newNode;
     }
+    // finally, link current node to the new node
+    current.next=newNode
+    /*
+    ->Visual:
+    Before:
+    [10] ⇄ [20] ⇄ [30]
+
+    Insert 25 at index 2:
+
+    After:
+    [10] ⇄ [20] ⇄ [25] ⇄ [30]
+    */
+  }
 }
