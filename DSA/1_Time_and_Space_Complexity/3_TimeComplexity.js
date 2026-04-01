8bea0768643c17b01e43b2a2e27eb0643c68c929
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-04-01 21:44:42 +0530

    O(1) examples

--- DSA/1_Time_and_Space_Complexity/3_TimeComplexity.js
+++ DSA/1_Time_and_Space_Complexity/3_TimeComplexity.js
@@ -1,5 +1,3 @@
-//To run this go inside this folder via cd and node --watch 3_TimeComplexity.js
-
 //Example someOperations: O(1)
 function someOperations(n){
     //3 operations
@@ -8,7 +6,8 @@ function someOperations(n){
 console.log(someOperations(2))
 console.log(someOperations(100))
 
-/*Here this function/algorithm have timeComlexity of O(1) because it was only one operation i.e constant number of operations,
+/*
+Here this function/algorithm have timeComlexity of O(1) because it has only one operation i.e constant number of operations,
  it doesn't matter the number of input i.e n
  Here the 'someOperations' function have 3 operations i.e n*  is one, n+5 is two and /2 is third operation
 
