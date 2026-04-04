be7b0223dcbb18013fc62719dba3531444cc338e
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-04-04 13:05:44 +0530

    reverse array question

---
+++ JS_Coding_Questions/46_Reverse_Array.js
@@ -0,0 +1,26 @@
+//Reverse an array:
+//[1,2,3,4,5] --->>> [5,4,3,2,1]
+
+const array=[1,2,3,4,5];
+const reverseArray=(array)=>{
+    if(!Array.isArray(array)) return false;
+    if(array.length===0)return []
+
+    //use th result when in question it's mentioned don't mutate original array
+    // let result=[...array]
+
+    let start=0; end=array.length-1
+    //end will have value less then array length and that will be index of last element in array
+    while(start<end){
+        [array[start],array[end]]=[array[end],array[start]];
+         /*
+        first iteration: arr[0],arr[8]=arr[8],arr[0] i.e arr[0] will be arr[8] so in index 0 will be index 8 i.e 0 replaced by 9 and so on...
+        */
+        end--;
+        start++
+    }
+    return array
+
+}
+console.log("Original:",array)
+console.log("Reverse:",reverseArray(array))
