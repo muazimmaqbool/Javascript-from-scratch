51bd071ae42597ac755e9e18e930ec968d635984
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-04-14 22:58:21 +0530

    hard questions

--- DSA/1_Time_and_Space_Complexity/10_Thoery_Questions.js
+++ DSA/1_Time_and_Space_Complexity/10_Thoery_Questions.js
@@ -93,81 +93,60 @@ Extra space used by algorithm excluding input
 👉 Example: let arr = new Array(n); // auxiliary space = O(n)
 
 12. What is in-place algorithm?
-
 ✅ Answer:
 An algorithm that uses O(1) extra space
-
 👉 Example: Swapping elements in same array
 
 🔴 Hard / Interview Level:
 
 13. Difference between worst-case and average-case complexity?
-
 ✅ Answer:
-
-Worst-case → maximum time taken
-Average-case → expected time for random input
-
-👉 Interviews usually focus on worst-case
+  Worst-case → maximum time taken
+  Average-case → expected time for random input
+  👉 Interviews usually focus on worst-case
 
 14. Why is O(log n) faster than O(n)?
-
 ✅ Answer:
 Because it reduces the problem size drastically.
-
 👉 Example:
+  n = 1,000,000
+  O(n) → 1,000,000 steps
+  O(log n) → ~20 steps 😳
 
-n = 1,000,000
-O(n) → 1,000,000 steps
-O(log n) → ~20 steps 😳
-15. What is amortized time complexity?
-
+15. What is a mortized time complexity?
 ✅ Answer:
 Average time per operation over multiple operations.
-
 👉 Example:
-
-Array push → O(1) amortized
+Array push → O(1) a mortized
 Sometimes resizing → O(n), but rare
-16. Can an algorithm have multiple complexities?
 
+16. Can an algorithm have multiple complexities?
 ✅ Answer: Yes
 👉 Example:
-
-O(n + log n) → O(n)
-
-We take the dominant term
+  O(n + log n) → O(n)
+  We take the dominant term
 
 17. What is trade-off between time and space?
-
 ✅ Answer:
 Sometimes you use more memory to reduce time.
-
 👉 Example:
-
 HashMap → faster lookup (O(1)) but uses extra space
-18. Why recursion uses more space?
 
+18. Why recursion uses more space?
 ✅ Answer:
 Because each function call is stored in call stack
-
 👉 Depth of recursion = space complexity
 
 19. What is best-case complexity?
-
 ✅ Answer:
 Minimum time required when input is ideal
+👉 Example: Searching first element → O(1)
 
-👉 Example:
-
-Searching first element → O(1)
 20. Why is Big-O important in interviews?
-
 ✅ Answer:
 Because it shows:
-
-Problem-solving efficiency
-Scalability thinking
-Ability to optimize code
+  Problem-solving efficiency
+  Scalability thinking
+  Ability to optimize code
 
 */
