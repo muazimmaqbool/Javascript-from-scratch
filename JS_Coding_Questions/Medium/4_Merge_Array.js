9f1308651a60165a54ece8ca2ae4ea623301106f
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-04-29 20:49:30 +0530

    time complexity of merge array

--- JS_Coding_Questions/Medium/4_Merge_Array.js
+++ JS_Coding_Questions/Medium/4_Merge_Array.js
@@ -22,17 +22,28 @@ Output :
     { id: 3, name: "Charlie" }
 */
 function mergeArray(arr1, arr2) {
+  // We use Map to store data in key-value format:
+  // key → id
+  // value → object
+  // This gives us O(1) lookup time when checking if an id exists
   const map = new Map();
+
+  //Add all elements from arr1 into the map
   for (let el of arr1) {
     map.set(el.id, { ...el });
   }
   for (let el of arr2) {
     if (map.has(el.id)) {
+      //If exists → merge old + new object
+      // ...el → new properties overwrite old ones if same key exists
       map.set(el.id, { ...map.get(el.id), ...el });
     } else {
       map.set(el.id, { ...el });
     }
   }
+  //Convert map values back to array
+  // map.values() returns an iterator (not a real array)
+  // Array.from converts it into a proper array
   return Array.from(map.values());
 }
 arr1 = [
@@ -43,7 +54,7 @@ arr2 = [
   { id: 2, age: 30 },
   { id: 3, name: "Charlie" },
 ];
-console.log("Example 1:",mergeArray(arr1, arr2));
+console.log("Example 1:", mergeArray(arr1, arr2));
 /*
     o/p; [
             { id: 1, name: 'Alice' },
@@ -53,11 +64,31 @@ console.log("Example 1:",mergeArray(arr1, arr2));
 */
 const arr3 = [
   { id: 1, name: "Ali" },
-  { id: 2, name: "John" }
-]
+  { id: 2, name: "John" },
+];
 
 const arr4 = [
-  { id: 1,age: 20 },
-  { id: 3, name: "Sara" }
-]
-console.log("Example 2:",mergeArray(arr3,arr4))
+  { id: 1, age: 20 },
+  { id: 3, name: "Sara" },
+];
+console.log("Example 2:", mergeArray(arr3, arr4));
+
+//Time Complexity is O(n+m)
+/*
+->First loop (arr1):
+    for (let el of arr1) runs n times so O(n)
+    map.set() is O(1)
+    So final is O(n)
+
+->Second loop (arr2):
+    for (let el of arr2) runs m times i.e O(m)
+    map.has() → O(1)
+    map.get() → O(1)
+    map.set() → O(1)
+    so final is O(m)
+
+->Convert Map to Array:
+    Array.from(map.values()) : Iterates through all map values, Map size in worst case = n + m = O(n + m)
+
+->Final Time Complexity: O(n) + O(m) + O(n + m) = O(n + m)
+*/
