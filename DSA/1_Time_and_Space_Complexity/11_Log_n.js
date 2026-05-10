89049d15d6ed2b2379a7bebc6b4da23134ad3dac
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-05-10 20:40:18 +0530

    binary search example of log n

--- DSA/1_Time_and_Space_Complexity/11_Log_n.js
+++ DSA/1_Time_and_Space_Complexity/11_Log_n.js
@@ -19,7 +19,28 @@ O(log n) means the work increases very slowly even if input becomes huge.
 
     That is O(log n).
 */
-//Example 1: Binary Search
+//Example 1: Binary Search:
+function binarySearch(arr, target) {
+  let left=0;
+  let right=arr.length-1;
+
+  while(left<=right){
+    let mid=Math.floor((left+right)/2)
+
+    if(arr[mid]===target){
+        return mid
+    }
+    if(arr[mid]<target){
+        left=mid+1
+    }else{
+        right=mid-1
+    }
+  }
+  return -1
+}
+
+console.log(binarySearch([1,2,3,4,5,6,7,8,9], 7)); // 6 i.e 7 is at index 6
+console.log("*************")
 //Example 2:
 function countByHalf(n) {
  while(n>1){
@@ -32,4 +53,5 @@ countByHalf(16); // 16, 8, 4, 2
 console.log("********")
 countByHalf(20); // 20, 10, 5, 2
 //Every iteration divides by 2. So time complexity = O(log n).
-//here space complexity is O(1) (you are only using: one variable n and No arrays, objects, or recursion stack. So memory usage stays constant. i.e Space Complexity = O(1))
+//here space complexity is O(1) 
+// (you are only using: one variable n and No arrays, objects, or recursion stack. So memory usage stays constant. i.e Space Complexity = O(1))
