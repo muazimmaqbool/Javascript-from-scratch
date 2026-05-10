76bb50e815ade03d6e8556b2562b0c3962deec41
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-05-10 20:35:30 +0530

    example of log n

--- DSA/1_Time_and_Space_Complexity/11_Log_n.js
+++ DSA/1_Time_and_Space_Complexity/11_Log_n.js
@@ -16,6 +16,20 @@ O(log n) means the work increases very slowly even if input becomes huge.
         again go middle
         again divide into half
     Every step cuts data into half.
-    
+
     That is O(log n).
 */
+//Example 1: Binary Search
+//Example 2:
+function countByHalf(n) {
+ while(n>1){
+    console.log(n)
+    n=Math.floor(n/2); //if n=5 i.e 5/2=2.5 but i will return 2 because of Math.floor()
+ }
+}
+
+countByHalf(16); // 16, 8, 4, 2
+console.log("********")
+countByHalf(20); // 20, 10, 5, 2
+//Every iteration divides by 2. So time complexity = O(log n).
+//here space complexity is O(n)
