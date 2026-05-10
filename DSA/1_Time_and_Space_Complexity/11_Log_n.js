f959860e02b03d7065317c7dd9acaa0685c69552
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-05-10 20:31:31 +0530

    O(log n) intro

---
+++ DSA/1_Time_and_Space_Complexity/11_Log_n.js
@@ -0,0 +1,21 @@
+//-> O(log n) Time Complexity:
+/*
+O(log n) means the work increases very slowly even if input becomes huge.
+->Usually happens when:
+    we divide the problem into half every step
+    Binary Search
+    Trees
+    Divide & Conquer
+
+->Real Life Example:
+    Imagine searching a word in a dictionary.
+    You don’t start from page 1.
+    You:
+        open middle page
+        decide left or right
+        again go middle
+        again divide into half
+    Every step cuts data into half.
+    
+    That is O(log n).
+*/
