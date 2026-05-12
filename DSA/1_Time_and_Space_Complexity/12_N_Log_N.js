1327192559134c7576ade986234e9b293183f99b
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-05-12 14:04:17 +0530

    merge sort example

--- DSA/1_Time_and_Space_Complexity/12_N_Log_N.js
+++ DSA/1_Time_and_Space_Complexity/12_N_Log_N.js
@@ -28,3 +28,38 @@ This is one of the most important complexities in DSA.
 
         That becomes O(n log n).
 */
+//Main Example — Merge Sort
+/*    Idea:
+        Divide array into halves
+        Sort each half
+        Merge them
+*/
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+
+  return merge(left, right);
+}
+function merge(left, right) {
+  let result = [];
+  let i = 0, j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      result.push(left[i]);
+      i++;
+    } else {
+      result.push(right[j]);
+      j++;
+    }
+  }
+
+  return result
+    .concat(left.slice(i))
+    .concat(right.slice(j));
+}
+
+console.log(mergeSort([8,3,5,1,9,6])); //[1,3,5,6,8,9]
