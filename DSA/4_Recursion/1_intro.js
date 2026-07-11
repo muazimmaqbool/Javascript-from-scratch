3db4341911395cf87a027bb454331a9d5bb43e4e
Muazim Maqbool <muazimmaqbool4@gmail.com>
2026-07-11 21:14:03 +0530

    recursion

--- DSA/4_Recursion/1_intro.js
+++ DSA/4_Recursion/1_intro.js
@@ -63,6 +63,7 @@ console.log(multiplyUsingRecursion([1,2,3,4])) // 24
  ->third: 2 * arr[1]
  ->fourth 1* arr[]
  so we have 4*3*2*1=24
+ 
 */
 
 //Note: It's upto to you whether you will use looping or recursion, just make sure code is always simple to read, somtimes based on problem loops is better sometimes recursion
